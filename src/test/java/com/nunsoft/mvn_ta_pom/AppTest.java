64d305144bc675abdbfa5189718deff51f75a413
MCayirli <cayirli@gmail.com>
2020-11-28 16:33:34 +0300

    Empty Project

---
+++ src/test/java/com/nunsoft/mvn_ta_pom/AppTest.java
@@ -0,0 +1,20 @@
+package com.nunsoft.mvn_ta_pom;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
